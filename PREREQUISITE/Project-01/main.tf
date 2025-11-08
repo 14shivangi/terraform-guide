304cd75213dfdb2168f9d82dede37e718ecd89bd
Shivangi Mishra <140191375+14shivangi@users.noreply.github.com>
2025-11-09 00:16:56 +0530

    Update main.tf

--- PREREQUISITE/Project-01/main.tf
+++ PREREQUISITE/Project-01/main.tf
@@ -5,4 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0abc0000000000000"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-000000000000000000"
+    key_name = "aws_signup"
 }

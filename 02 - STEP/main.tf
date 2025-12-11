d2e12f7d1a9717587360231b09e47971965822f9
Shivangi Mishra <140191375+14shivangi@users.noreply.github.com>
2025-12-11 12:13:37 +0530

    Update main.tf

--- 02 - STEP/main.tf
+++ 02 - STEP/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-0202920020902090828090" # replace this
   instance_type_value = "t2.micro"
   subnet_id_value = "subnet-072827389992029252e7". # replace this
 }

6455ca37dca4e8663710e2e594fb0d8e458545de
Shivangi Mishra <140191375+14shivangi@users.noreply.github.com>
2025-12-17 00:52:13 +0530

    Update main.tf

--- 02 - STEP/module-EC2-instance/main.tf
+++ 02 - STEP/module-EC2-instance/main.tf
@@ -1,1 +1,10 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_instance" "name" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id_value
+    associate_public_ip_address = true
+}

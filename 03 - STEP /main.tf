297fbe7beefa58f23ef8d405aec8bc5e55d92918
Shivangi Mishra <140191375+14shivangi@users.noreply.github.com>
2025-12-27 00:00:12 +0530

    Change AWS region and instance resource name

--- 03 - STEP /main.tf
+++ 03 - STEP /main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "ec2instance" {
   instance_type = "t2.micro"
   ami = "ami-053b0d53c279acc90" # change this
   subnet_id = "subnet-019ea91ed9b5252e7" # change this

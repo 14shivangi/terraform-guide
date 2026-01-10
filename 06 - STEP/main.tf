cbba49d63cc15369499c02b1daa49416ded865b7
Shivangi Mishra <140191375+14shivangi@users.noreply.github.com>
2026-01-10 11:53:40 +0530

    Update main.tf

--- 06 - STEP/main.tf
+++ 06 - STEP/main.tf
@@ -1,1 +1,32 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+provider "vault" {
+  address = "<>:8200"
+  skip_child_token = true
+
+  auth_login {
+    path = "auth/approle/login"
+
+    parameters = {
+      role_id = "<>"
+      secret_id = "<>"
+    }
+  }
+}
+
+data "vault_kv_secret_v2" "mysecretv" {
+  mount = "secret" // change it according to your mount
+  name  = "test-secret" // change it according to your secret
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-00000000000000000000"  #change it according to your ami
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test"
+    Secret = data.vault_kv_secret_v2.example.data["foo"]
+  }
+}
